83681eb49611e4642f6bffb8a1fe9235317eea16
Matthew Holloway <matthew@springload.co.nz>
2020-07-20 20:53:38 +1200

    Updates

---
+++ precache-manifest.5b81871cd6a163c20341c3ca3d7c7921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "885ffd57f301c4b90afbec4780053743",
+    "url": "/react-extract-jsx/index.html"
+  },
+  {
+    "revision": "4b54884ff45ebd847fa4",
+    "url": "/react-extract-jsx/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5297925f509280ee84da",
+    "url": "/react-extract-jsx/static/js/2.d4c6bde7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-extract-jsx/static/js/2.d4c6bde7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b54884ff45ebd847fa4",
+    "url": "/react-extract-jsx/static/js/main.42c70fa2.chunk.js"
+  },
+  {
+    "revision": "1fc68fedb8440963c9e1",
+    "url": "/react-extract-jsx/static/js/runtime-main.9ca46846.js"
+  }
+]);
